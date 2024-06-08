cb4e14386a49fd587e4c4ea2a810498c75ade7b6
Punn Nararatwong <punn.nara@gmail.com>
2024-06-08 17:05:56 +0700

    Fix og image path

--- const.ts
+++ const.ts
@@ -3,4 +3,4 @@ export const BLOG_URL = '/blog';
 export const BLOGS_URL = '/blogs';
 export const TAG_URL = '/tag';
 export const PROJECTS_URL = '/projects';
-export const DEFAULT_OG_IMAGE = 'https://punn.dev/default-og.jpg';
+export const DEFAULT_OG_IMAGE = 'https://punn.dev/default-og.png';
